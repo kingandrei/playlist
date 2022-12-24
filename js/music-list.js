e769ccaef81a3098732cdf3448b3661a6e81afe8
kingandrei <113972149+kingandrei@users.noreply.github.com>
2022-12-24 23:21:06 +0800

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,67 @@
+let allMusic = [
+
+    {
+        name: "Paraluman",
+        artist: "Adie",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Tahanan",
+        artist: "Adie",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Sweet Nothing",
+        artist: "Taylor Swift",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Labyrinth",
+        artist: "Taylor Swift",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Out of my League",
+        artist: "Stephen Speaks",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Best Part",
+        artist: "Daniel Caesar",
+        img: "music-6",
+        src: "music-6"
+    },
+
+    {
+        name: "Favourite Clothes",
+        artist: "Rini",
+        img: "music-7",
+        src: "music-7"
+    },
+
+    {
+        name: "Sunday Morning",
+        artist: "Maroon 5",
+        img: "music-8",
+        src: "music-8"
+    },
+
+    {
+        name: "Good Days",
+        artist: "Sza",
+        img: "music-9",
+        src: "music-9"
+    },
+    
+    {
+        name: "Mahika",
+        artist: "Adie and Janine Berdin",
+        img: "music-10",
+        src: "music-10"
+    },
+    ];
